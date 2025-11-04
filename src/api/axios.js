4e41f8a827e72305d8c2a197d1d9085d146a7abb
Felix Díaz <felix.diaz.b@tecsup.edu.pe>
2025-11-04 11:57:58 -0500

    Conecta API de producción

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
-const API_URL = os.environ.get('VITE_API_URL', 'https://peliculas-backend.onrender.com/api/');
-
 const apiClient = axios.create({
-    baseURL: API_URL
+    baseURL: 'https://peliculas-backend-zj33.onrender.com/api/'
 });
 
 export default apiClient;
